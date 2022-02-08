e8b8c64706a1ff02709812313f188e0edec4b1ab
mjmacarty <matt.macarty@gmail.com>
2022-02-08 13:27:27 -0500

    demo files

--- loan_app.py
+++ loan_app.py
@@ -2,8 +2,9 @@ from loan_analyst import Loan
 import sys
 from time import sleep
 
-#TODO allow user to edit loan parameters
-#TODO allow comparison of two loans
+
+# TODO allow user to edit loan parameters
+# TODO allow comparison of two loans
 
 def display_menu():
     print("""
@@ -19,40 +20,47 @@ def display_menu():
     8. Quit
     """)
 
+
 def new_loan(rate, term, pv):
     loan = Loan(rate, term, pv)
     print("Loan initialized")
     sleep(.5)
     return loan
 
+
 def pmt(loan):
     print(loan.pmt_str)
 
+
 def amort(loan):
-    print(loan.table)    
+    print(loan.table)
+
 
 def summary(loan):
     loan.summary()
 
+
 def plot(loan):
     loan.plot_balances()
+
 
 def pay_faster(loan):
     amt = float(input("Enter extra monthly payment: "))
     new_term = loan.pay_early(amt)
     print(f"{new_term} years")
 
+
 def pay_early(loan):
     years_to_pay = int(input("Enter years to debt free: "))
     result = loan.retire_debt(years_to_pay)
-    print(f"Monthly extra: {result[0]:,.2f} \tTotal Payment: {result[1]:,.2f}")    
+    print(f"Monthly extra: {result[0]:,.2f} \tTotal Payment: {result[1]:,.2f}")
 
-action = {'1' : new_loan, '2' : pmt,  '3': amort, '4': summary,
-          '5': plot, '6': pay_early, '7' : pay_faster}
+
+action = {'1': new_loan, '2': pmt, '3': amort, '4': summary,
+          '5': plot, '6': pay_early, '7': pay_faster}
+
 
 def main():
-    
-    
     while True:
         display_menu()
         choice = input("Enter your selection: ")
@@ -60,21 +68,19 @@ def main():
             rate = float(input("Enter interest rate: "))
             term = int(input("Enter loan term: "))
             pv = float(input("Enter amount borrowed: "))
-            loan = Loan(rate, term,pv)    
+            loan = Loan(rate, term, pv)
             print("Loan initialized")
             sleep(.75)
-            
+
         elif choice in '234567':
             action[choice](loan)
-            sleep(1)    
+            sleep(1)
         elif choice == '8':
             print("Goodbye")
             sys.exit()
         else:
-            print("please enter a valid selection")        
-
+            print("please enter a valid selection")
 
 
 if __name__ == "__main__":
     main()
-    
